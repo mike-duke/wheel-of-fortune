5a1de6f5b78d518d9ad582a641737840e9d808ed
Derek Hanciles <drhanciles@icloud.com>
2018-10-16 17:40:28 -0600

    Cleaned up unused code and comments

--- test/Player-test.js
+++ test/Player-test.js
@@ -1,13 +1,7 @@
 const chai = require('chai'); 
 const expect = chai.expect; 
 const Player = require('../lib/Player.js'); 
-// const spies = require('chai-spies'); 
-// chai.use(spies); 
-
-// global.domUpdates = require('../lib/domUpdates.js'); 
-
-// chai.spy.on(global.domUpdates, [], () => true); 
-
+ 
 describe('Player', () => {
 let player; 
 
@@ -17,18 +11,6 @@ beforeEach(() => {
 
 it('should instatiate a new Player class', () => {
 expect(player).to.be.an.instanceof(Player); 
-})
-
-it('should be able to guess letters to towards solving the puzzle', () => {
-
-}), 
-
-it('should be able to buy a vowel', () => {
-
-}), 
-
-it('should be able to solve the puzzle', () => {
-    
 })
 
 });
