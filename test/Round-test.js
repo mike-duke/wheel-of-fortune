5b33d7e7aa141ce70ec893fd975beb29cd458834
Derek Hanciles <drhanciles@icloud.com>
2018-10-15 20:56:35 -0600

    Wheel value test has been implemented in round test file

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,11 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Round = require('../lib/Round.js');
-// const spies = require('chai-spies');
-// chai.use(spies);
+const spies = require('chai-spies');
+chai.use(spies);
 
-// global.domUpdates = require('../lib/domUpdates.js');
-// chai.spy.on(global.domUpdates, [], () => true);
+global.domUpdates = require('../lib/domUpdates.js');
+chai.spy.on(global.domUpdates, ['displayWheelValue'], () => true);
 
 describe('Round', function() {
 
@@ -17,6 +17,16 @@ describe('Round', function() {
 
   it('should instantiate a new Round class', () => {
     expect(round).to.be.an.instanceof(Round);
+  }); 
+  
+  it('should return the value of the current wheel selection', () => {
+
+    let result = round.getWheelValue()
+    
+    expect(result).to.equal(700)
+    
+    expect(domUpdates.displayWheelValue).to.have.been.called(1); 
+    expect(domUpdates.displayWheelValue).to.have.been.called.with(700)
   })
 
 })
